46a4905ddd98f8c21d43bae730ff8efa1d34d149
Andjela <andjela.petrovic@softelm.com>
2022-02-09 11:30:06 +0100

    'message'

---
+++ app/Http/Controllers/V1/OrderController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$orders=Order::find(9)->food; //show food for order id=9
+
+        $orders=DB::table('orders')         //Show all food for all orders
+            ->join('food_order','order_id','=','food_order.order_id')
+            ->join('food','food_id','=' , 'food.id')
+            ->select('food.name')
+            ->get();
+
+
+
+        return response()->json($orders, 201);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $storeOrder = Order::create($request->all());
+
+        $storeOrder->save();
+        $input['food'] = $request->input('food');
+        $storeOrder->food()->attach($input['food']);
+
+        return $storeOrder;
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
