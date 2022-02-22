29c1df46598a3308cca5c3895300164c3f739455
Andjela <andjela.petrovic@softelm.com>
2022-02-22 09:15:42 +0100

    Adding in CategoryController.php index

--- app/Http/Controllers/V1/CategoryController.php
+++ app/Http/Controllers/V1/CategoryController.php
@@ -16,6 +16,7 @@ class CategoryController extends Controller
     public function index()
     {
         $category=Category::all();
+        $category=Category::all();
         return $category;
     }
 
